f32201f9b6aad7cc692fbc5981b0e2a62ddd6fc3
Tim <tim@timwheeler.com>
2019-01-11 13:46:08 -0500

    working on BankAccount app

--- Constructors/src/com/timwheeler/BankAccount.java
+++ Constructors/src/com/timwheeler/BankAccount.java
@@ -1,90 +1,127 @@
 package com.timwheeler;
 
+import java.util.Random;
+
 public class BankAccount {
 
     private int accountNumber = 0;
+    private int accountPin = 0;
     private boolean hasAccountNumber = false;
-    private double accountBalance = 100.00;
+    private double accountBalance = 0;
+    private double initialDeposit = 0;
     private String customerName = null;
     private String email = null;
     private String phoneNumber = null;
 
     public boolean loginSuccessful = false;
 
+    public BankAccount(String _customerName, String _email, String _phoneNumber, double _initialDeposit) {
+        this.accountNumber = this.generateAccountNumber();
+        this.initialDeposit = _initialDeposit;
+        this.accountBalance += _initialDeposit;
+        this.customerName = _customerName;
+        this.email = _email;
+        this.phoneNumber = _phoneNumber;
+
+        hasAccountNumber = true;
+        loginSuccessful = true;
+    }
+
+    public int generateAccountNumber(){
+        Random rand = new Random();
+        accountNumber = rand.nextInt(999999999);
+        return accountNumber;
+    }
+
+    public int generateAccountPin(){
+        Random rand = new Random();
+        accountPin = rand.nextInt(9999);
+        return accountPin;
+    }
+
+    public void logout() {
+        System.out.println("You've been logged out!");
+    }
+
+    public void login(int _accountNumber, int _accountPin){
+        if (_accountNumber == this.accountNumber && _accountPin == this.accountPin) {
+            System.out.println("Login successful");
+            this.getAccountInfo();
+        }
+    }
 
     public void setAccountNumber(int _accountNumber) {
         if (_accountNumber == 5680420 || _accountNumber == 5850257) {
-            this.accountNumber = _accountNumber;
-            this.hasAccountNumber = true;
-            this.loginSuccessful = true;
-            System.out.println(this.accountNumber + " successfully logged in!");
+            accountNumber = _accountNumber;
+            hasAccountNumber = true;
+            loginSuccessful = true;
+            System.out.println(accountNumber + " successfully logged in!");
         } else {
             System.out.println("Login failed!");
         }
-
     }
 
     public void setCustomerName(String _customerName) {
-        this.customerName = _customerName;
+        customerName = _customerName;
     }
 
     public void setEmail(String _email) {
-        this.email = _email;
+        email = _email;
     }
 
     public void setPhoneNumber(String _phoneNumber) {
-        this.phoneNumber = _phoneNumber;
+        phoneNumber = _phoneNumber;
     }
 
     public int getAccountNumber() {
-        System.out.println("Acct #: " + this.accountNumber);
-        return this.accountNumber;
+        System.out.println("Acct #: " + accountNumber);
+        return accountNumber;
     }
 
     public double getAccountBalance() {
-        System.out.println("Balance: " + this.accountBalance);
-        return this.accountBalance;
+        System.out.println("Balance: " + accountBalance);
+        return accountBalance;
     }
 
     public String getCustomerName() {
-        System.out.println("Customer: " + this.customerName);
-        return this.customerName;
+        System.out.println("Customer: " + customerName);
+        return customerName;
     }
 
     public String getEmail() {
-        System.out.println("Email: " + this.email);
-        return this.email;
+        System.out.println("Email: " + email);
+        return email;
     }
 
     public String getPhoneNumber() {
-        System.out.println("Phone: " + this.phoneNumber);
-        return this.phoneNumber;
+        System.out.println("Phone: " + phoneNumber);
+        return phoneNumber;
     }
 
     public void getAccountInfo() {
-        getCustomerName();
         getAccountNumber();
+        getCustomerName();
         getAccountBalance();
         getEmail();
         getPhoneNumber();
     }
 
     public void depositFunds(double _funds) {
-        if (this.hasAccountNumber) {
-            this.accountBalance += _funds;
-            System.out.println("Your account balance is " + this.accountBalance);
+        if (hasAccountNumber) {
+            accountBalance += _funds;
+            System.out.println("Your account balance is " + accountBalance);
         } else {
             System.out.println("Please enter your account number!");
         }
     }
 
     public void withdrawFunds(double _funds) {
-        if (this.hasAccountNumber) {
-            if ((this.accountBalance - _funds) < 0) {
-                System.out.println("Insufficient funds! \nCurrent Balance: " + this.accountBalance);
+        if (hasAccountNumber) {
+            if ((accountBalance - _funds) < 0) {
+                System.out.println("Insufficient funds! \nCurrent Balance: " + accountBalance);
             } else {
-                this.accountBalance -= _funds;
-                System.out.println("Your account balance is " + this.accountBalance);
+                accountBalance -= _funds;
+                System.out.println("Your account balance is " + accountBalance);
             }
         } else {
             System.out.println("Please enter your account number!");
--- Constructors/src/com/timwheeler/Main.java
+++ Constructors/src/com/timwheeler/Main.java
@@ -6,30 +6,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount();
+        boolean infoIsCorrect = false;
+        boolean loggedIn = false;
+        BankAccount account = null;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your account number: ");
-        int accountNumber = scanner.nextInt();
-        scanner.close();
 
-        account.setAccountNumber(accountNumber);
+        System.out.println("Hello, welcome to Block Bank. What is your name?");
+        String customerName = scanner.nextLine();
 
-        if (account.loginSuccessful) {
+        System.out.println("Nice to meet you, " + customerName + ". Let's get your new account set up.");
 
-            account.setCustomerName("Tim Wheeler");
-            account.setEmail("tim@timwheeler.com");
-            account.setPhoneNumber("(774) 245-6079");
 
-            account.depositFunds(100);
+        System.out.println("What is your email address?");
+        String email = scanner.nextLine();
 
+        System.out.println("Great! And your phone number?");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Excellent. How much will your initial deposit be?");
+        double initialDeposit = scanner.nextDouble();
+
+        System.out.println("Perfect. I have your information as:\nName: " + customerName + "\nEmail: " + email + "\nPhone: " + phoneNumber + "\nCould you please verify that this information is correct?");
+        System.out.println("Enter true or false");
+        infoIsCorrect = scanner.nextBoolean();
+
+        if (infoIsCorrect) {
+            account = new BankAccount(customerName, email, phoneNumber, initialDeposit);
             account.getAccountInfo();
+            loggedIn = true;
 
-//            account.withdrawFunds(233.33);
+        } else {
+            System.out.println("Apologies. Let's try this again");
+            scanner.close();
         }
 
 
-//        System.out.println();
+        if (loggedIn) {
+            account.depositFunds(177);
+            account.logout();
+        }
 
     }
 }
