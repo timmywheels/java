ecc60aa36ee86cfbc31ca412249c9c57b82eaddb
Tim <tim@timwheeler.com>
2019-02-03 22:43:39 -0500

    patriots win their 6th super bowl!!!!!!

--- finance-scraper/src/main/java/Auth.java
+++ finance-scraper/src/main/java/Auth.java
@@ -2,58 +2,105 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Auth {
 
 
-    public static void webClient(String url, String firstName, String lastName, String email, String phone, String message) {
-
+    public static void webClient(String loginUrl, String username, String password, String portfolioUrl) {
         System.setProperty("webdriver.gecko.driver", "/Library/Java/Extensions/geckodriver");
         WebDriver driver = new FirefoxDriver();
         WebDriverWait wait = new WebDriverWait(driver, 30);
 
+        try {
 
-        try{
-            driver = new FirefoxDriver();
-            driver.navigate().to(url);
-
+//          1. GOTO YAHOO FINANCE LOGIN URL
+            driver.navigate().to(loginUrl);
             System.out.println("Page Title: " + driver.getTitle());
             System.out.println("Page URL: " + driver.getCurrentUrl());
 
-            WebElement formTriggerButton = driver.findElement(By.xpath("/html/body/div/div/div/div/section/div/div/div/a/button"));
-            formTriggerButton.click();
-            System.out.println("Form trigger button clicked!");
+        } catch (Exception e) {
 
-            WebElement firstNameInput = driver.findElement(By.xpath("//*[@id=\"firstNameInput\"]"));
-            firstNameInput.sendKeys(firstName);
-
-            WebElement lastNameInput = driver.findElement(By.xpath("//*[@id=\"lastNameInput\"]"));
-            lastNameInput.sendKeys(lastName);
-
-            WebElement emailInput = driver.findElement(By.xpath("//*[@id=\"emailInput\"]"));
-            emailInput.sendKeys(email);
-
-            WebElement phoneInput = driver.findElement(By.xpath("//*[@id=\"phoneInput\"]"));
-            phoneInput.sendKeys(phone);
-
-            WebElement messageInput = driver.findElement(By.xpath("//*[@id=\"messageInput\"]"));
-            messageInput.sendKeys(message);
-
-            WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div/section/div[2]/div/form/div/input[8]"));
-            submitButton.click();
-            System.out.println("Form submitted successfully!");
-        } catch(Exception e) {
-            System.out.println("Cannot open Firefox!");
+            System.out.println("Could not find URL!");
             e.printStackTrace();
             driver.close();
         }
 
 
+        try {
+//          2. ENTER USERNAME
+            WebElement usernameInput = driver.findElement(By.id("login-username"));
+            usernameInput.sendKeys(username);
+            System.out.println("Username entered...");
+
+        } catch (Exception e) {
+            System.out.println("Error entering username!");
+            e.printStackTrace();
+            driver.close();
+        }
 
 
+        try {
+//          3. CLICK 'NEXT' BUTTON
+            WebElement nextButton = driver.findElement(By.id("login-signin"));
+            nextButton.click();
+            System.out.println("'Next' button clicked...");
 
+        } catch (Exception e) {
+            System.out.println("Error hitting 'next' button!");
+            e.printStackTrace();
+            driver.close();
+        }
+
+
+        try {
+//          4. ENTER PASSWORD
+            WebElement passwordInput = driver.findElement(By.id("login-passwd"));
+            passwordInput.sendKeys(password);
+            System.out.println("Password entered...");
+
+        } catch (Exception e) {
+            System.out.println("Error entering password!");
+            e.printStackTrace();
+            driver.close();
+        }
+
+
+        try {
+//          5. CLICK 'SIGN-IN' BUTTON
+            WebElement signInButton = driver.findElement(By.id("login-signin"));
+            signInButton.click();
+            System.out.println("'Sign-in' button clicked...");
+
+        } catch (Exception e) {
+            System.out.println("Error hitting 'sign-in' button!");
+            e.printStackTrace();
+            driver.close();
+        }
+
+
+        try {
+//          6. NAVIGATE TO PORTFOLIO
+            driver.navigate().to(portfolioUrl);
+
+        } catch (Exception e) {
+            System.out.println("Could not navigate to portfolio!");
+            e.printStackTrace();
+            driver.close();
+        }
+
+
+        try {
+//          7. SCRAPE TABLE
+            WebElement tableRow = driver.findElement(By.xpath("//tr"));
+            System.out.println("Table Row: " + tableRow);
+
+            System.out.println("Page: " + driver.getPageSource());
+
+        } catch (Exception e) {
+            System.out.println("Could not scrape table!");
+            e.printStackTrace();
+            driver.close();
+        }
     }
 }
--- finance-scraper/src/main/java/Main.java
+++ finance-scraper/src/main/java/Main.java
@@ -1,48 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-
-            String baseUrl = "https://finance.yahoo.com" ;
+            String baseUrl = "https://finance.yahoo.com";
             String loginUrl = "https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F";
-            String login = "tim@timwheeler.com";
+            String username = "tim@timwheeler.com";
             String password = "mvp71111" ;
+            String portfolioUrl = "https://finance.yahoo.com/portfolio/p_0/view";
 
-            String url = "https://timwheeler.com";
+            Auth.webClient(loginUrl, username, password, portfolioUrl);
 
-            String firstName = "Timothy";
-            String lastName = "Wheeler";
-            String email = "bot@bot.com";
-            String phone = "774-245-6079";
-            String message = "I am a bot. Go Patriots!";
-
-
-            Auth.webClient(url, firstName, lastName, email, phone, message);
-
-
-
-
-//            try {
-//
-//                System.out.println("Starting autoLogin on " + loginUrl);
-//                webClient client = AuthHTMLUnit.autoLogin(loginUrl, login, password);
-//                HtmlPage page = client.getPage(baseUrl);
-//                String logoutLink = "https://login.yahoo.com/config/?logout_all=1";
-//
-//
-//                if(logoutLink != null ){
-//                    System.out.println("Successfully logged in!");
-//                    // printing the cookies
-//                    for(Cookie cookie : client.getCookieManager().getCookies()){
-//                        System.out.println(cookie.toString());
-//                    }
-//
-//                } else {
-//                    System.err.println("Wrong credentials");
-//                }
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
         }
 
     }
