8ff4fba6ed9df37c02891a3eef2d97acf392c985
Tim Wheeler <tim@timwheeler.com>
2019-02-08 11:04:24 -0500

    Update Main.java

--- finance-scraper/src/main/java/Main.java
+++ finance-scraper/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
             String baseUrl = "https://finance.yahoo.com";
             String loginUrl = "https://login.yahoo.com/config/login?.intl=us&.lang=en-US&.src=finance&.done=https%3A%2F%2Ffinance.yahoo.com%2F";
             String username = "tim@timwheeler.com";
-            String password = "mvp71111" ;
+            String password = "Scraper2019!!" ;
             String portfolioUrl = "https://finance.yahoo.com/portfolio/p_0/view/v1";
 
             Auth.webClient(loginUrl, username, password, portfolioUrl);
